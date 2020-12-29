84c8b39400a3512c8fa88680b92676f928f01624
ganariya <willwill1997willwill@yahoo.co.jp>
2020-12-29 10:31:32 +0900

    simple change

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='gym_md',
-      version='0.0.1',
-      install_requires=['gym']  # And any other dependencies foo needs
-      )
+setup(
+    name='gym_md',
+    version='0.0.1',
+    description='OpenAI Gym Environment for MiniDungeons',
+    url='https://github.com/Ganariya/gym-md',
+    author='ganariya',
+    author_email='ganariya2525@gmail.com',
+    license='MIT',
+    # packages=find_packages(where='gym_md'),
+    packages=[
+        'gym_md',
+        'gym_md.envs'
+    ],
+    install_requires=['gym']
+)
