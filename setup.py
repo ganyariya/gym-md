f35949d08795058a995de5a4fe04c07bbf4809d3
ganariya <willwill1997willwill@yahoo.co.jp>
2020-12-29 11:22:37 +0900

    simple change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gym_md',
-    version='0.0.1',
+    version='0.0.2',
     description='OpenAI Gym Environment for MiniDungeons',
     url='https://github.com/Ganariya/gym-md',
     author='ganariya',
