8f2d3d35614c2f99ee4eec1fa79c1bd7a9f79d84
ganariya <ganariya2525@gmail.com>
2021-10-24 12:50:13 +0900

    :sparkles: 体力と攻撃力に初期値を設定する

--- gym_md/envs/config/props_config.py
+++ gym_md/envs/config/props_config.py
@@ -7,9 +7,15 @@ from pydantic import BaseModel
 class PropsConfig(BaseModel):
     """基本設定."""
 
-    PLAYER_MAX_HP: int
-    ENEMY_POWER: int
-    PORTION_POWER: int
+    PLAYER_MAX_HP: int = 40
+    IS_PLAYER_HP_LIMIT: bool = False
+
+    ENEMY_POWER: int = 10
+    ENEMY_POWER_MIN: int = 10
+    ENEMY_POWER_MAX: int = 10
+    IS_ENEMY_POWER_RANDOM: bool = False
+
+    PORTION_POWER: int = 10
     DISTANCE_INF: int
     RENDER_WAIT_TIME: float
 
